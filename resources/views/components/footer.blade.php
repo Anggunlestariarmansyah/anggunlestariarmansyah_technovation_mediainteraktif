94f738c981e3e744c229f12bc82089c4412617f2
Anggunlestariarmansyah <anggunlestariarman@gmail.com>
2024-07-07 03:10:02 +0800

    Commit Pertama

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-dark text-light text-center py-2">
+    <p class="m-0">
+        Made with ❤ by <a class="text-light" href="https://github.com/anggun">anggun</a>
+    </p>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+@section('content')
+
+
+
+
+<div class="d-flex justify-content-center align-items-center" style="background-image: url({{ asset('img/bg.png')}}); height: 100vh; background-position: center; background-size: cover;">
+    <div class="d-flex justify-content-center flex-column">
+    <h1 class="display-5 fw-bold text-center">Warnetku</h1>
+        <p class="text-center">
+            Internet cafe (warnet) management system
+        </p>
+        <a class="btn btn-dark btn-lg text-center" href="" target="_blank" rel="noopener noreferrer">
+            Source Code
+        </a>
+    </div>
+</div>
+
+
+
+
+@endsection
